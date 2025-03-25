cfac308cf3e8cde9a12a3f4ce37ac0d133b0f5a1
komal vishwakarma <1408komal@gmail.com>
2025-03-25 22:03:39 +0530

    Added first maven project for java

---
+++ src/main/java/gfg32maven/gfg32maven/App.java
@@ -0,0 +1,13 @@
+package gfg32maven.gfg32maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/gfg32maven/gfg32maven/MyCalc.java
@@ -0,0 +1,42 @@
+package gfg32maven.gfg32maven;
+
+public class MyCalc {
+	public int sum(int a, int b)
+	{
+		return a+b;
+	}
+	public int diff(int a, int b)
+	{
+		return (a-b);
+	}
+	public int mul(int a, int b)
+	{
+		return (a*b);
+	}
+	
+//	public int div(int a, int b)
+//	{
+//		return (a/b);
+//	}
+//	public int mod(int a, int b)
+//	{
+//		return (a%b);
+//	}
+//	public void greet()
+//	{
+//		System.out.println("Hello Master Branch");
+//	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyCalc calc = new MyCalc();
+		System.out.println("Sum is :"+calc.sum(20, 10));
+		System.out.println("Diff is :"+calc.diff(20, 10));
+		System.out.println("Mul is :"+calc.mul(20, 10));
+//		System.out.println("Div is :"+calc.div(20, 10));
+//		System.out.println("Mod is :"+calc.div(20, 10));
+//		calc.greet();
+
+	}
+
+}
